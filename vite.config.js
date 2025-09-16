7bfc6a989cd9e7644fedf67ecdd13c4090c79cae
Mona Fakhri <96212168+fakhrisalem@users.noreply.github.com>
2025-09-16 09:07:52 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/flowers/",
+ base: '/Paradise-Nursery-Shopping-Application/',
   plugins: [react()],
   // Add other configurations if necessary
 })
